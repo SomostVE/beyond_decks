2c7ae7c14f95a73473eac33a68c228378b619d59
SomostVE <66364264+SomostVE@users.noreply.github.com>
2026-08-23 22:35:07 +0200

    chore: remove obsolete Battle Sim header patch

--- js/battle-link.js
+++
@@ -1,9 +0,0 @@
-const actions = document.querySelector(".header-actions");
-
-if (actions && !actions.querySelector('[href="./battle.html"]')) {
-  const link = document.createElement("a");
-  link.className = "button page-nav-button";
-  link.href = "./battle.html";
-  link.textContent = "Battle Sim";
-  actions.insertBefore(link, actions.firstChild);
-}
